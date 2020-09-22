88f5b6c455a2de8b3cb60bbbe88be979a339b0b1
lzymac <javalzy@126.com>
2020-09-22 13:56:39 +0800

    Merge remote-tracking branch 'upstream/master'

--- mergeFromRemote.sh
+++ mergeFromRemote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git remote -v
-git remote add upstream https://github.com/TarsCloud/TarsFramework.git
+git remote add --track master upstream https://github.com/TarsCloud/TarsFramework.git
 git fetch upstream
 git merge upstream/master
 git push
